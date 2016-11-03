03a4efc3cfebb56b1d0ea14cac2dfc6577bf39bc
tunnckoCore <mameto_100@mail.bg>
2016-11-03 16:54:43 +0200

    test(init): add tests

--- index.js
+++ index.js
@@ -8,19 +8,18 @@
 'use strict'
 
 var fs = require('fs')
-var app = require('create-readdir-stream')
+var Readdir = require('create-readdir-stream').CreateReaddirStream
 var path = require('path')
 var through2 = require('through2')
 var writeFile = require('write-file')
 
 module.exports = function streamCopyDir (src, dest, plugin) {
+  var app = new Readdir()
   return app.createReaddirStream(src)
     .pipe(through2.obj(function (file, enc, cb) {
       fs.stat(file.path, function (err, stats) {
         /* istanbul ignore next */
-        if (err) {
-          return cb(err)
-        }
+        if (err) return cb(err)
 
         file.stat = stats
         file.contents = null
@@ -31,18 +30,29 @@ module.exports = function streamCopyDir (src, dest, plugin) {
 
         fs.readFile(file.path, function (err, str) {
           /* istanbul ignore next */
-          if (err) {
-            return cb(err)
-          }
+          if (err) return cb(err)
 
           file.contents = str
 
           if (typeof plugin === 'function') {
-            plugin(file)
+            var called = false
+            plugin(file, function (er, f) {
+              called = true
+              /* istanbul ignore next */
+              if (er) return cb(er)
+              end(f)
+            })
+
+            /* istanbul ignore next */
+            return called ? null : end(file)
           }
 
-          var fp = path.resolve(dest, file.basename)
-          writeFile(fp, file.contents, cb)
+          function end (file) {
+            var fp = path.resolve(dest, file.basename)
+            writeFile(fp, file.contents, cb)
+          }
+
+          end(file)
         })
       })
     }))
--- test.js
+++ test.js
@@ -8,3 +8,48 @@
 /* jshint asi:true */
 
 'use strict'
+
+var fs = require('fs')
+var test = require('mukla')
+var mkdirp = require('mkdirp')
+var rimraf = require('rimraf')
+var copyDir = require('./index')
+
+test('should not copy directories and skip them', function (done) {
+  rimraf.sync('./tmp')
+  mkdirp.sync('./tmp/actual/foobar')
+  mkdirp.sync('./tmp/dest')
+
+  fs.writeFileSync('./tmp/actual/qux.txt', 'some content')
+
+  copyDir('./tmp/actual', './tmp/dest')
+    .once('error', done)
+    .once('finish', function () {
+      var len = fs.readdirSync('./tmp/dest').length
+      test.strictEqual(len, 1)
+      rimraf.sync('./tmp')
+      done()
+    })
+})
+
+test('should allow customizing file content through plugin', function (done) {
+  rimraf.sync('./abc')
+  mkdirp.sync('./abc/zzz')
+
+  fs.writeFileSync('./abc/zzz/ddd.json', 'aa{{name}}cc')
+
+  copyDir('./abc/zzz', './abc', function (file, cb) {
+    var contents = file.contents.toString()
+    contents = contents.replace('{{name}}', 'BBB')
+    file.contents = new Buffer(contents)
+    cb(null, file)
+  })
+    .once('error', done)
+    .once('finish', function () {
+      var data = fs.readFileSync('./abc/ddd.json', 'utf8')
+      test.strictEqual(data, 'aaBBBcc')
+      rimraf.sync('./abc')
+      done()
+    })
+})
+
